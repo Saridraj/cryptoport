9a6d67285a7c993218334184eb3f64c88beae138
Saridraj <saridrajsapsin@gmail.com>
2024-06-21 17:22:53 +0700

    docker setup

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Dockerfile
+
+# Use an official Node.js runtime as a parent image
+FROM node:16
+
+# Set the working directory
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application
+COPY . .
+
+# Build the NestJS application
+RUN npm run build
+
+# Expose the port the app runs on
+EXPOSE 4000
+
+# Define the command to run the app
+CMD ["npm", "run", "start"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,28 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { typeOrmConfig } from './config/typeorm.config';
+
 import { PortfolioModule } from './portfolio/portfolio.module';
 import { CryptoDataModule } from './crypto-data/crypto-data.module';
+import { User } from './entity/user.entity';
+import { Portfolio } from './entity/portfolio.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User, Portfolio],
+      synchronize: true,
+    }),
     AuthModule,
     PortfolioModule,
-    CryptoDataModule
+    CryptoDataModule,
   ],
   controllers: [AppController],
   providers: [AppService],
